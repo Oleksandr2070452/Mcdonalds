fddf22ec62cf7a25cbf6932ba972e6694cb60dc8
DashaYab <dash.yab@yahoo.com>
2023-05-17 17:19:36 +0300

    add autotest SPAC32

--- src/test/java/tests/TestMcDonaldsDasha.java
+++ src/test/java/tests/TestMcDonaldsDasha.java
@@ -11,7 +11,7 @@ import static java.lang.Thread.sleep;
 
 public class TestMcDonaldsDasha extends TestInit {
 
-    @Test
+    @Test(description = "SPAC-31")
     public void checkSearchField() throws InterruptedException {
         driver.findElement(By.xpath("//span[@class='item-text']")).click();
         driver.findElement(By.xpath("//input[@id='form-text-1673594539']")).sendKeys("BigMac");
@@ -23,5 +23,16 @@ public class TestMcDonaldsDasha extends TestInit {
         for (WebElement el : searchResults) {
             Assert.assertTrue(el.getText().toLowerCase().contains("біг мак"));
         }
+    }
+
+    @Test(description = "SPAC-32")
+    public void checkBtnWorkPlace() throws InterruptedException {
+        WebElement workPlaceBtn = driver.findElement(By.xpath("//span[contains(text(),'Робочі місця')]"));
+        workPlaceBtn.click();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("/working_places.html"));
+
+        WebElement workPlaceTitle = driver.findElement(By.xpath("//h1[contains(text(),'Робочі місця')]"));
+        Assert.assertTrue(workPlaceTitle.isDisplayed());
     }
 }
