5433cc7202317ea848f3560f6d7d473c391058bb
Oleksandr Kraglyk <sanek2070452@gmail.com>
2023-05-19 10:47:25 +0300

    C:/Program Files/Git/SPAC-51

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -15,6 +15,7 @@ public class HomePage extends BasePage {
     public static final String ARROW_IN_SLIDER_MOVE_NEXT = "//button[@aria-label='Next']";
     public static final String LEARN_MORE_BTN = "//*[@id='teaser-4fd499e808']/div[2]/div[2]/a";
     public static final String FACEBOOK_BTN = "//img[@alt='Facebook']";
+    public static final String TIK_TOK_BTN = "//img[@alt='Tiktok']";
 
     public WebElement getWorkInMcdonalds() {
         return driver.findElement(By.xpath(WORK_IN_MCDONALDS_BTN));
@@ -39,5 +40,19 @@ public class HomePage extends BasePage {
                 (driver.getWindowHandles());
         driver.switchTo().window(switchToWindow.get(1));
         return this;
+    }
+
+    public WebElement getTikTokBtn() {
+        return driver.findElement(By.xpath(TIK_TOK_BTN));
+    }
+
+    public HomePage redirectionOnTikTokPage() {
+        scrollForElement(getTikTokBtn(), driver);
+        getTikTokBtn().click();
+        ArrayList<String> switchToWindow = new ArrayList<>
+                (driver.getWindowHandles());
+        driver.switchTo().window(switchToWindow.get(1));
+        return this;
+
     }
 }
