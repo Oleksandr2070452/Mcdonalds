9dd666bb5d803defa319db301c7336aa7f0a44ef
Vadym <vadym2001n@gmail.com>
2023-05-24 12:06:59 +0300

    SPAC-22_check_button_tender

--- src/test/java/tests/TestButtonTender.java
+++ src/test/java/tests/TestButtonTender.java
@@ -1,13 +1,15 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TestButtonTender extends TestInit{
+public class TestButtonTender extends TestInit {
     @Test(description = "SPAC-22")
     public void checkButtonTender() throws InterruptedException {
         driver.findElement(By.xpath("(//span[contains(text(),'Тендери')])[1]")).click();
         driver.findElement(By.xpath("//a[contains(text(),'Завантажити')]")).click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("/mcdonalds_in_ukraine/tenders.html"));
     }
 }
 
