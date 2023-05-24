fbcdc346f16a5aa49fecb9fdf622be16bd033004
Vadym <vadym2001n@gmail.com>
2023-05-24 13:35:31 +0300

    SPAC-23_срÑcheck_button_consumer_corner

--- src/test/java/tests/TestButtonConsumerCorner.java
+++ src/test/java/tests/TestButtonConsumerCorner.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestButtonConsumerCorner extends TestInit {
@@ -9,5 +10,6 @@ public class TestButtonConsumerCorner extends TestInit {
         driver.findElement(By.xpath("(//a[contains(text(),'Якість та гігієна')])[2]")).click();
         driver.findElement(By.xpath("(//a[contains(text(),'Дізнатись більше')])[2]")).click();
         driver.findElement(By.xpath("//a[contains(text(),'Куточок споживача')]")).click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("/dam/sites/ua/nfl/pdf/Kutochok.pdf"));
     }
 }
